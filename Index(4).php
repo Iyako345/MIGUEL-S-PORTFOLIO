9917122caaf6bf02d0a1fbb9846ff116c41fca4d
Iyako345 <abdalisesaldaba@yahoo.com>
2026-05-29 00:31:04 +0800

    Add files via upload

---
+++ Index(4).php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Website</title>
+    <link rel="stylesheet" type="text/css" href="STYLE.css">
+</head>
+<body>
+   
+    <h1>Miguel's Portfolio</h1> 
+    <div class="nav">
+    <ul> 
+<p>
+ <h1><a href="Home.html"> Home</a></h1>
+   <h1><a href="About.html"> About</a></h1>
+  <h1><a href="Contact.html"> Contact</a></h1>
+ <h1><a href="Gallery.html">Gallery</a></h1> 
+</p>
+
+
+</ul>
+</div>
+
+</body>
+</html>
